042d0c6ecf18943266695fc4e4564b129ed46274
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-25 17:55:36 +1000

    Add carousel options

--- sandbox.js
+++ sandbox.js
@@ -2,3 +2,12 @@ $('.nav li').on('click', function(){
     $(this).parent().children().removeClass('active');
     $(this).addClass('active');
 });
+
+$(function(){
+    $('.carousel').carousel({
+        interval: 2000,
+        pause: true,
+        wrap: false,
+        keyboard: true
+    });
+});
